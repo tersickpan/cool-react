f6701ba11cd929b42e9687da323c66f162d68500
tersickpan <sickfan21@gmail.com>
2025-07-16 23:02:57 +0800

    fix bugs and added vids

--- src/components/base/BaseVideoPreview.jsx
+++ src/components/base/BaseVideoPreview.jsx
@@ -1,16 +1,24 @@
-export default function BaseVideoPreview({ src, volume = 1.0, muted = true }) {
+export default function BaseVideoPreview({
+  src,
+  volume = 1.0,
+  muted = true,
+  loop = true,
+  className = "",
+  ...props
+}) {
   if (!src) return;
 
   return (
-    <div className="max-w-md mx-auto">
+    <div className={`max-w-md mx-auto ${className}`}>
       <video
         src={src}
         controls
         autoPlay
-        loop
+        loop={loop}
         muted={muted}
         volume={volume}
         className="rounded-2xl shadow-xl w-full"
+        {...props}
       />
     </div>
   );
